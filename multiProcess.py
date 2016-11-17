b3770c45230c65c921e281093dc542fbb2cdeeb0
aditya-vk <adityavk711@gmail.com>
2016-11-17 16:39:55 -0500

    minor modifications and cleanup

--- multiProcess.py
+++ multiProcess.py
@@ -1,8 +1,8 @@
 # A sample program to run multiple processes
 
-# ##########################
-# # Start Preliminary Code #
-# ##########################
+# ############### #
+#  Start of Code  # 
+# ############### #
 
 # Start both the processes at a certain value, say 0.
 # Pause one at 0.1 sec and another at 0.2 sec
@@ -17,7 +17,7 @@ def slow(q):
 		q.put(i)
 		print i
 		i+=1
-		time.sleep(0.2)
+		time.sleep(0.3)
 		if i == 10:
 			break
 
@@ -30,29 +30,35 @@ def fast(q):
 		time.sleep(0.1)
 		if i == 10:
 			break
+			# Signal the other process to terminate
 
+# Main
 if __name__ == '__main__':
+	# Initialize the queues
 	q0 = multiprocessing.Queue()
 	q1 = multiprocessing.Queue()
 
+	# Initialize the processes
 	p0 = multiprocessing.Process(target = slow, args = (q0,))
 	p1 = multiprocessing.Process(target = fast, args = (q1,))
 
+	# Start the processes
 	p0.start()
 	p1.start()
 
+	# Collect the queue
 	p0.join()
 	p1.join()
 	
-
+	# Put a sentinel at the end of the queue
 	q0.put(-1)
 	q1.put(-1)
 	v0 = []
 	v1 = []
 
+	# Acquire the queue data
 	checker0 = 1
 	checker1 = 1
-
 	while checker0 > 0:
 		checker0 = q0.get()
 		v0.append(checker0)
@@ -60,25 +66,20 @@ if __name__ == '__main__':
 		checker1 = q1.get()
 		v1.append(checker1)
 	
+	# Print the queue
 	print 'values in 1st queue', v0[0:len(v0)-1]
 	print 'values in 2nd queue', v1[0:len(v1)-1]
 
 
 
-# ########################
-# # End Preliminary Code #
-# ########################
-
-# Notes from Preliminary: The values from each process is not obtained in order, which is fine for our case.
+# ############# #
+#  End of Code  #
+# ############# #
 
 
 
 
-
-
-
-
-
+# Taking user input to stop a process: useful code
 
 # import sys
 # import select
@@ -98,19 +99,6 @@ if __name__ == '__main__':
 # 	print 'no input'
 # 	time.sleep(1)
 
-
-
-
-
-
-
-
-
-
-#######
-# In the following code, PROBLEM is that the queue is getting cleaned in every loop -> Turns out this happens even otherwise
-# when the process generates multiple values, only the first one is being stored in the queue. 
-#######
 
 # import multiprocessing as mp
 # import time
@@ -139,3 +127,17 @@ if __name__ == '__main__':
 # 	p.join()
 # 	values = q0.get()
 # 	print 'The queue is made of: ', values
+
+
+###################
+# IMPORTANT NOTES #
+###################
+
+# The child is not notified of the death of its parent, ***it only works the other way***.
+# However, when a process dies all its file descriptors are closed. And the other end of a pipe is notified about this,
+# if it selects it for reading.
+
+# So your parent can create a pipe before spawning the process (or in fact, you can just set up stdin to be a pipe),
+# and the child can select that for reading. It will report ready for reading when the parent end is closed. 
+# This requires your child to run a mail loop, or at least make regular calls to select. 
+# If you don't want that, you'll need some manager process to do it, but then when that one is killed, things break again.
