d9b3151e6094a11babc8005bf185eb359dde9f4b
aditya-vk <adityavk711@gmail.com>
2016-12-07 23:41:54 -0500

    changed the graph

--- buildTree.py
+++ buildTree.py
@@ -9,10 +9,10 @@ H = nx.path_graph(5)
 G.add_nodes_from(H)
 
 # Add the edges
-G.add_edges_from([(0,1),(0,2),(1,3),(2,4)])
+G.add_edges_from([(0,1),(0,2),(1,3),(2,4),(3,5),(5,7),(4,6),(6,8)])
 
-# 0 -> 1 -> 3
-# 	-> 2 -> 4
+# 0 -> 1 -> 3 -> 5 -> 7
+# 	-> 2 -> 4 -> 6 -> 8
 
 # Save the grpah for later use
 nx.write_gpickle(G,"G.gpickle")
